a8dd8dad5d2305e97105376db278f0e15c33f0f6
vden <vden@ugramail.ru>
2015-05-02 21:49:46 +0500

    02052015

--- wwwgaseng/pto_objekt/views.py
+++ wwwgaseng/pto_objekt/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.views import generic
+from django.core.urlresolvers import reverse
+from pto_objekt.models import osnovanie , objekt
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'pto/index.html'
+    #context_object_name = 'latest_question_list'
+    model = objekt
+    #def get_context_data(self, **kwargs):
+        #return generic.ListView.get_context_data(self, **kwargs)
+ #   def get_queryset(self):
+ #       return objekt.objects.ge_naimenovaniekr
--- wwwgaseng/wwwgaseng/urls.py
+++ wwwgaseng/wwwgaseng/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns('',                       
     # Examples:
     # url(r'^$', 'wwwgaseng.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    #url(r'^pto/', include('pto_objekt.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('ge.urls')),
+    #url(r'^ge/', include('ge.urls')),
+    #url(r'^pto/', include('pto.urls')),
+    
 )
+
